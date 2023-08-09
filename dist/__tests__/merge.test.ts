578f6bdf3629a2bfac8e08155671cc262128eb8c
hazelnope <nopnprii@gmail.com>
2023-08-09 14:57:11 +0700

    first commit

---
+++ dist/__tests__/merge.test.ts
@@ -0,0 +1,12 @@
+// "use strict";
+// const { merge } = require('../merge');
+import { merge } from '../merge'
+
+describe('merge function', () => {
+    it('should merge two sorted arrays', () => {
+        const collection_1 = [1, 3, 5];
+        const collection_2 = [2, 4, 6];
+        const merged = merge(collection_1, collection_2);
+        expect(merged).toEqual([1, 2, 3, 4, 5, 6]);
+    });
+});
